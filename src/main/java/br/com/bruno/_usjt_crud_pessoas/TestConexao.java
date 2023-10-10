8fe086e0343cf8960fe3ad80ca6557bf2b93d9c2
Bruno Dias <brunogd148@gmail.com>
2023-10-09 21:17:24 -0300

    ap10(db): mensagem de erro add

--- src/main/java/br/com/bruno/_usjt_crud_pessoas/TestConexao.java
+++ src/main/java/br/com/bruno/_usjt_crud_pessoas/TestConexao.java
@@ -4,14 +4,21 @@
  */
 package br.com.bruno._usjt_crud_pessoas;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author 82311358
  */
 public class TestConexao {
     public static void main(String[] args) throws Exception {
+       try{
         var factory = new ConnectionFactory();
         var minhaConexao = factory.conectar();
         System.out.println("Conectou!!");
+    }    
+    catch(Exception e) {
+    JOptionPane.showMessageDialog(null, "Conexão falhou!  Erro: " + e);   
+       }       
     }
 }
